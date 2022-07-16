b4c0f05fa089bd6782d30572300ccb7bc55471ac
Shaq <n9996095@qut.edu.au>
2022-07-16 17:18:48 +1000

    stuff works

--- eyeFiltering.py
+++ eyeFiltering.py
@@ -1,17 +1,32 @@
 import cv2
 import numpy as np
+import dlib
 
 video = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+# Blob detection
+detector = dlib.get_frontal_face_detector()
+
+def error_print(i):
+    face_error = "None"
+    if(i == 0):
+        face_error = "No face detected"
+    if(i == 1):
+        face_error = "Face detected"
+    print(face_error)
+
 while True:
     ret, frame = video.read()
+    frame_width, frame_height = frame.shape[:2]
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
     gray_picture = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#make picture gray
     faces = face_cascade.detectMultiScale(gray_picture, 1.1, 5)
+    error = 0
 
     for (x,y,w,h) in faces:
+        error = 1
         #cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),5)
         gray_face = gray_picture[y:y+h, x:x+w] # cut the gray face frame out
         face = frame[y:y+h, x:x+w] # cut the face frame out
@@ -21,10 +36,34 @@ while True:
             cv2.rectangle(frame,(x+ex,y+ey),(x+ex+ew,y+ey+eh),(0,0,255),7)'''
         #eye_frame = frame[eyes[0][0]:eyes[1][0]+eyes[1][2], eyes[0][1]:eyes[1][1]+eyes[1][3]]
 
-    h,w = face.shape[:2]
-    cut_face = face[round(h/5):round(3*h/5), 0:w]
-    cv2.imshow("webcam", cut_face)
+        h,w = face.shape[:2]
+        cut_face = face[round(h/4):round(h/2), 0:w]
+        eye_resized = cv2.resize(cut_face,(frame_width*3, frame_height//2))
+        eye_resized = eye_resized[:, :eye_resized.shape[1]//2]
+        
+        rows, cols, _ = eye_resized.shape
+        cut_face_grey = cv2.cvtColor(eye_resized,cv2.COLOR_BGR2GRAY)
+        filtered_eyes = cv2.GaussianBlur(cut_face_grey, (3, 3), 0)
+        _, threshold = cv2.threshold(filtered_eyes, 25, 255, cv2.THRESH_BINARY_INV)
+        contours, _  = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
+    
+        for cnt in contours:
+            (x, y, w, h) = cv2.boundingRect(cnt)
+            cv2.drawContours(eye_resized, [cnt], -1, (0, 0, 255), 3)
+            cv2.line(eye_resized, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
+            cv2.line(eye_resized, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
+            break
+
+
+        cv2.imshow("webcam", eye_resized)
+
+        # blob detection 
+        #cv2.imshow("webcam", eyes_with_keypoints)
+
+
+    error_print(error)
     c = cv2.waitKey(1)
     if c == 27:
         break
--- test.py
+++ test.py
@@ -1,57 +1,32 @@
-from __future__ import print_function
-from __future__ import division
-import cv2 as cv
+import cv2
 import numpy as np
-import argparse
-import random as rng
-rng.seed(12345)
-def thresh_callback(val):
-    threshold = val
-    
-    canny_output = cv.Canny(src_gray, threshold, threshold * 2)
-    
-    
-    _, contours, _ = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    
-    # Get the moments
-    mu = [None]*len(contours)
-    for i in range(len(contours)):
-        mu[i] = cv.moments(contours[i])
-    # Get the mass centers
-    mc = [None]*len(contours)
-    for i in range(len(contours)):
-        # add 1e-5 to avoid division by zero
-        mc[i] = (mu[i]['m10'] / (mu[i]['m00'] + 1e-5), mu[i]['m01'] / (mu[i]['m00'] + 1e-5))
-    # Draw contours
-    
-    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-    
-    for i in range(len(contours)):
-        color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-        cv.drawContours(drawing, contours, i, color, 2)
-        cv.circle(drawing, (int(mc[i][0]), int(mc[i][1])), 4, color, -1)
-    
-    
-    cv.imshow('Contours', drawing)
-    
-    # Calculate the area with the moments 00 and compare with the result of the OpenCV function
-    for i in range(len(contours)):
-        print(' * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f' % (i, mu[i]['m00'], cv.contourArea(contours[i]), cv.arcLength(contours[i], True)))
 
-parser = argparse.ArgumentParser(description='Code for Image Moments tutorial.')
-parser.add_argument('--input', help='Path to input image.', default='stuff.jpg')
-args = parser.parse_args()
-src = cv.imread("Photos/IMG_2301.jpg")
-if src is None:
-    print('Could not open or find the image:', args.input)
-    exit(0)
-# Convert image to gray and blur it
-src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-src_gray = cv.blur(src_gray, (3,3))
-source_window = 'Source'
-cv.namedWindow(source_window)
-cv.imshow(source_window, src)
-max_thresh = 255
-thresh = 100 # initial threshold
-cv.createTrackbar('Canny Thresh:', source_window, thresh, max_thresh, thresh_callback)
-thresh_callback(thresh)
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    roi = frame[269: 795, 537: 1416]
+    rows, cols, _ = roi.shape
+    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+
+    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+    _, threshold = cv2.threshold(gray_roi, 3, 255, cv2.THRESH_BINARY_INV)
+    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+    for cnt in contours:
+        (x, y, w, h) = cv2.boundingRect(cnt)
+        #cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
+        cv2.line(roi, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
+        break
+
+    cv2.imshow("Threshold", threshold)
+    cv2.imshow("gray roi", gray_roi)
+    cv2.imshow("Roi", roi)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cv2.destroyAllWindows()
